a33f6ecd325d37b4d46cc80fea431e36f24ece89
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 07:20:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     puts WIN_COMBINATIONS.length
-    
+    winning_combo = []
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -29,9 +29,11 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      winning_combo << win_combination
+      winning_combo
     else
-      return false
+      false
     end
   end
+  
 end
