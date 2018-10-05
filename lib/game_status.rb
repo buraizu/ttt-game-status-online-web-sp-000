2db133cc5132dbb4f62ddb46a3677f5e8a9aa20e
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 07:21:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      winning_combo << win_combination
+      winning_combo.push(win_combination) 
       winning_combo
     else
       false
