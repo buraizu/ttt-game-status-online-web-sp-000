5e3cb126461d51b5513afb82adb63f006e51f0f8
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 07:33:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,10 +29,9 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
-    else
-      return false
-    end
     end
   end
-
+  return false
 end
+
+
